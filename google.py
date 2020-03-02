71e44ef017a9979b6be6b9f6c5a9a687f2a34bab
zhangsn <398816613@qq.com>
2020-03-02 09:46:30 +0800

    Update google.py

--- google.py
+++ google.py
@@ -29,7 +29,7 @@
 
 # ############## Local Domain Settings ##############
 my_host_name = '{DOMAIN}' # Modified by zmirror-onekey
-my_host_scheme = 'http://' # Modified by zmirror-onekey
+my_host_scheme = 'https://' # Modified by zmirror-onekey
 my_host_port = None  # None表示使用默认端口, 可以设置成非标准端口, 比如 81
 
 # ############## Target Domain Settings ##############
@@ -45,6 +45,7 @@ external_domains = (
     'images.google.com.hk',
     'images.google.com',
     'apis.google.com',
+    'ogs.google.com.hk',
 
     # Google学术
     'scholar.google.com.hk',
